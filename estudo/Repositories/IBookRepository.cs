99187243b3680f019d74b22d76a15b5ea85233ef
targinu <targino26@outlook.com>
2023-10-27 18:48:03 -0300

    criando repositories

--- estudo/Repositories/IBookRepository.cs
+++ estudo/Repositories/IBookRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace estudo.Repositories
+﻿using estudo.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace estudo.Repositories
 {
     public interface IBookRepository
     {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task<Book> Update(Book book);
+
+        Task<Book> Delete(int id);
     }
 }
--- estudo/Startup.cs
+++ estudo/Startup.cs
@@ -1,4 +1,5 @@
 using estudo.Model;
+using estudo.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,7 @@ namespace estudo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BookContext>(x => x.UseSqlite("Data source=books.db"));
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
